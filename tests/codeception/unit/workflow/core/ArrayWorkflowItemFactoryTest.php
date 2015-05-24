5e6dff0b99f222f5890350f6d29ccdcd16dcd929
Bui Sy Nguyen <nguyenbs@projectkit.net>
2015-05-25 03:24:10 +0700

    Update test case for #5

--- tests/codeception/unit/workflow/core/ArrayWorkflowItemFactoryTest.php
+++ tests/codeception/unit/workflow/core/ArrayWorkflowItemFactoryTest.php
@@ -131,7 +131,7 @@ class ArrayWorkflowItemFactoryTest extends TestCase
 
     /**
      * @expectedException fproject\workflow\core\WorkflowException
-     * @expectedExceptionMessage Failed to load workflow definition : Class app\models\SomethingSource does not exist
+     * @expectedExceptionMessage Failed to load workflow definition : Class tests\codeception\unit\models\SomethingSource does not exist
      */
     public function testGetStatusFromDynamicDefinitionFail1()
     {
