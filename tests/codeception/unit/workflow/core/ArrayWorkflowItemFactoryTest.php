7abf2c5e352ca8a4311895ffcf293f09e84d27d4
Bui Sy Nguyen <nguyenbs@projectkit.net>
2015-05-28 03:14:46 +0700

    Fix bug testcase

--- tests/codeception/unit/workflow/core/ArrayWorkflowItemFactoryTest.php
+++ tests/codeception/unit/workflow/core/ArrayWorkflowItemFactoryTest.php
@@ -3,6 +3,7 @@
 namespace tests\unit\workflow\core;
 
 use Codeception\Specify;
+use Codeception\Util\Debug;
 use fproject\workflow\core\ActiveWorkflowBehavior;
 use fproject\workflow\core\ArrayWorkflowItemFactory;
 use fproject\workflow\core\WorkflowException;
@@ -285,6 +286,8 @@ class ArrayWorkflowItemFactoryTest extends TestCase
 
     private function checkStatusArray($wfId, $expectedStatus, $resultStatus, $reverse=false)
     {
+        Debug::debug($expectedStatus);
+        Debug::debug($resultStatus);
         $equals = true;
         foreach($expectedStatus as $key => $value)
         {
